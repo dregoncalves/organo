04d3ad22b48e612de48bb24dfa63a33898ed9e96
Andre Gonçalves <dregoncalves22@gmail.com>
2024-08-05 16:26:17 -0300

    Hooks e componente de times

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,13 +3,13 @@ import './CampoTexto.css'
 const CampoTexto = (props) => {
 
     const aoDigitado = (evento) => {
-        console.log(evento.target.value);
+        props.aoAlterado(evento.target.value)
     }
 
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
